63fb1039392f1c1a30ea666cff59b86d8edfbc99
Rodrigo Zamora <71241681+rodrigo-zamora@users.noreply.github.com>
2022-10-27 19:21:42 -0500

    added pages and routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:app/screens/automations.dart';
+import 'package:app/screens/home.dart';
+import 'package:app/screens/statistics.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -8,15 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      routes: {
+        '/': (context) => const HomePage(),
+        '/automations': (context) => const AutomationsPage(),
+        '/statistics': (context) => const StatisticsPage(),
+      },
     );
   }
 }
